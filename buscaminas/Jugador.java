4f5698ec853d8a3b8d7c1a7238acd82b61102075
Azucena27 <61021132+Azucena27@users.noreply.github.com>
2021-06-11 00:12:12 -0500

    Add files via upload

---
+++ buscaminas/Jugador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+/**
+ *
+ * @author Azucena
+ */
+
+public class Jugador {
+    private String username;
+    
+    public Jugador(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
